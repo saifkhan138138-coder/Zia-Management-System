b7ab1577231a02fe3587d2d0151ee3f43abb17c3
saifkhan138138-coder <saifkhan138138@gmail.com>
2026-07-25 19:57:20 +0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,16 @@
+function login(){
 
+let user=document.getElementById("username").value;
+let pass=document.getElementById("password").value;
+
+if(user==="admin" && pass==="12345"){
+
+window.location="dashboard.html";
+
+}else{
+
+alert("Wrong Username or Password");
+
+}
+
+}
