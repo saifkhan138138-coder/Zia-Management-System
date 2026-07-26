2d213d9deb6495a83490f975bb8de2dfbdbf366b
saifkhan138138-coder <saifkhan138138@gmail.com>
2026-07-26 12:52:25 +0500

    Update records.js

--- records.js
+++ records.js
@@ -124,3 +124,107 @@ No Matching Record
 return;
 
   }
+result.forEach((record,index)=>{
+
+let expense =
+Number(record.food||0)+
+Number(record.maintenance||0)+
+Number(record.fuel||0)+
+Number(record.electricity||0)+
+Number(record.salary||0)+
+Number(record.rent||0)+
+Number(record.other||0);
+
+table.innerHTML += `
+
+<tr>
+
+<td>${record.date}</td>
+
+<td>${record.customer}</td>
+
+<td>Rs. ${record.sale}</td>
+
+<td>Rs. ${expense}</td>
+
+<td>Rs. ${record.cash}</td>
+
+<td>
+
+<button class="save-btn"
+onclick="viewRecord(${index})">
+👁 View
+</button>
+
+<button class="save-btn"
+onclick="editRecord(${index})">
+✏ Edit
+</button>
+
+<button class="clear-btn"
+onclick="deleteRecord(${index})">
+🗑 Delete
+</button>
+
+</td>
+
+</tr>
+
+`;
+
+});
+
+}
+
+function viewRecord(index){
+
+let data =
+JSON.parse(localStorage.getItem("zmsRecords")) || [];
+
+let record = data[index];
+
+alert(
+
+"Date : " + record.date +
+
+"\nCustomer : " + record.customer +
+
+"\nSale : Rs. " + record.sale +
+
+"\nCash : Rs. " + record.cash
+
+);
+
+}
+
+function editRecord(index){
+
+alert("Edit feature will be added in Version 2");
+
+}
+
+function deleteRecord(index){
+
+let data =
+JSON.parse(localStorage.getItem("zmsRecords")) || [];
+
+if(confirm("Delete this record?")){
+
+data.splice(index,1);
+
+localStorage.setItem(
+"zmsRecords",
+JSON.stringify(data)
+);
+
+loadRecords();
+
+}
+
+}
+
+window.onload=function(){
+
+loadRecords();
+
+};
