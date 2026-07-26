0f9f918fbdbffe15a3250706b3a10f2bfc4a3753
saifkhan138138-coder <saifkhan138138@gmail.com>
2026-07-26 12:04:56 +0500

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -4,5 +4,19 @@ function saveRecord() {
 
     localStorage.setItem("zmsRecord", today);
 
+    document.getElementById("savedRecord").innerHTML =
+        "Last Saved Record: " + today;
+
     alert("Record Saved Successfully!");
+}
+
+window.onload = function () {
+
+    let record = localStorage.getItem("zmsRecord");
+
+    if(record){
+        document.getElementById("savedRecord").innerHTML =
+            "Last Saved Record: " + record;
+    }
+
 }
