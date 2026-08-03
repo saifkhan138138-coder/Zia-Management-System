bd3d5de354df25425ddebd707d51328091d66718
saifkhan138138-coder <saifkhan138138@gmail.com>
2026-08-03 23:09:10 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    document.getElementById("todaySale").innerText = "Rs. 0";
+    document.getElementById("customers").innerText = "0";
+    document.getElementById("expenses").innerText = "Rs. 0";
+    document.getElementById("cash").innerText = "Rs. 0";
+
+});
