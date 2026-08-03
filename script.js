71462fbacc5653c1aef3b8c41e91bacc08d1195b
saifkhan138138-coder <saifkhan138138@gmail.com>
2026-08-03 23:16:57 +0500

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("cash").innerText = "Rs. 0";
 
 });
+document.getElementById("saveBtn").addEventListener("click", function () {
+
+    const record = {
+        date: document.getElementById("date").value,
+        staff: document.getElementById("staff").value,
+        timeIn: document.getElementById("timeIn").value,
+        timeOut: document.getElementById("timeOut").value,
+        sale: document.getElementById("sale").value,
+        customer: document.getElementById("customer").value,
+        due: document.getElementById("due").value,
+        food: document.getElementById("food").value,
+        maintenance: document.getElementById("maintenance").value,
+        fuel: document.getElementById("fuel").value,
+        cashInHand: document.getElementById("cashInHand").value
+    };
+
+    localStorage.setItem("zmsRecord", JSON.stringify(record));
+
+    alert("Record Saved Successfully!");
+});
